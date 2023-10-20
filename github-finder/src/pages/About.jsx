f3abe22a54c38c19c9f1b3c8d243e898babb28a5
unoriginal1234 <eric.t.roth@gmail.com>
2023-10-20 07:44:15 -0700

    added layout files

---
+++ github-finder/src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p>App to search Github users from Brad Traversy Udemy Course</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
